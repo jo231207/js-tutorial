91043aed57caef7fb8da0400197e4c329c1bd2c2
jo231207 <153150354+jo231207@users.noreply.github.com>
2024-05-23 15:23:04 +0900

    바닐라 JS로 크롬앱 만들기 4강 완료

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-console.log(h1);
+// const loginInput = loginForm.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
-h1.addEventListener("click", handleTitleClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-function handleTitleClick(){
-    h1.classList.toggle("clicked");    
+function onLoginSubmit(event){
+    
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerHTML = `Hello ${username}`;
+    greeting.innerText
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem("username", username);
+    
 }
+function paintGreetings(){
+    greeting.innerText = `Hello ${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else{
+    paintGreetings();
+}
+
+
+// function onLoginSubmit(){
+//     const value = loginInput.value;
+//     if(value === ""){
+//         alert("Please write your name.");
+//     } else if(value.length > 15){
+//         alert("Your name is too long.");
+//     }
+// }
+
+// loginButton.addEventListener("click", onLoginSubmit);
+
